74538e56057386249926085a3822bbf0109735c7
Phonicavi <mardpazin7san@hotmail.com>
2016-05-25 14:06:57 +0800

    init

---
+++ System/Basic.py
@@ -0,0 +1,78 @@
+from sklearn.externals import joblib
+import sys
+import os
+import helper
+import numpy as np
+import pandas as pd
+
+
+DATA_DIR = "../data/"
+
+class MarketPortfolio:
+	"""
+		class for Stock:000016
+		SHANGZHENG-50
+
+	"""
+	def __init__(self):
+		try:
+			filename = DATA_DIR+"000016_sz.csv"
+			self.raw = np.array(pd.read_csv(filename))
+			(self._m, self._n)  = self.raw.shape
+		except Exception, e:
+			print Exception,":",e
+		# expected return
+		self.ROR = self.getROR()
+		joblib.dump(self,'MarketPortfolio.mdl',compress = 3)
+
+	def getROR(self, item=6, interval=1):
+		"""item: 6-Adj Close"""
+		return np.array([float(self.raw[i, item]-self.raw[i+interval, item])/self.raw[i+interval, item] for i in range(self._m-interval)])
+
+
+class Stock:
+	"""class for Stock"""
+	def __init__(self, SN):
+		SN_head = SN / 1000
+		# Shanghai Stock Exchange - A
+		assert(SN_head == 600 or SN_head == 601 or SN_head == 603)
+		self.SN = int(SN)
+		try:
+			filename = DATA_DIR+str(SN)+"_ss.csv"
+			self.raw = np.array(pd.read_csv(filename))
+			(self._m, self._n)  = self.raw.shape
+			self.market = joblib.load('MarketPortfolio.mdl')
+		except Exception, e:
+			print Exception,":",e
+		# features
+		self.Volality10 = self.getVolality()
+		self.EarningPerShare = self.getEarningPerShare()
+		self.dailyROR = self.getROR()
+		self.marketROR = self.market.ROR
+		self.HighROR = self.getROR(item=2)
+		self.LowROR = self.getROR(item=3)
+		self.WilliamsR = self.getWilliamsR()
+
+	def getVolality(self, item=6, interval=10):
+		"""item: 6-Adj Close, interval: 10days"""
+		return np.array([self.raw[i:i+interval, item].std() for i in range(self._m-interval)])
+
+	def getROR(self, item=6, interval=1):
+		"""item: 6-Adj Close"""
+		return np.array([float(self.raw[i, item]-self.raw[i+interval, item])/self.raw[i+interval, item] for i in range(self._m-interval)])
+
+	def getEarningPerShare(self, item=6, interval=1):
+		"""item: 6-Adj Close"""
+		return np.array([float(self.raw[i, item]-self.raw[i+interval, item]) for i in range(self._m-interval)])
+
+	def getWilliamsR(self, item=4):
+		"""1-Open, 2-High, 3-Low, item: 4-Close"""
+		return np.array([float(self.raw[i, 2]-self.raw[i, 4])*100/max(0.01, self.raw[i, 2]-self.raw[i, 3]) for i in range(self._m)])
+
+
+if __name__ == '__main__':
+	stk1 = Stock(600050)
+	print stk1.SN
+	print stk1.Volality10
+	stk1.getVolality()
+
---
+++ System/helper.py
@@ -0,0 +1,25 @@
+import os
+from operator import itemgetter
+
+
+
+def sort_genre(Dict):
+	"""
+		sort dictionary by values
+
+	"""
+	return sorted(Dict.items(), key=itemgetter(1), reverse=True)
+
+
+def load(filename):
+	f = file(filename, 'r+')
+	result = eval(f.read())
+	f.close()
+	return result
+
+
+def save(item, filename):
+	f = file(filename, 'w+')
+	f.write(str(item))
+	f.close()
+
